3ce58e759a22acf2eabe0990d53bdf3ad3dfc5fc
kmnemon <ke@ke-imac.local>
2018-04-03 18:55:35 +0800

    test2

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+x = np.linspace(0, 1, 1000000)
+
+plt.xlim((0, 1))
+plt.ylim((-10, 10))
+
+y1 = -np.log(x)   #1
+y2 = -np.log(1-x)  #0
 
 
-x =lambda : 3 > 2
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.ylabel('lose')
+plt.xlabel('y hat')
+plt.title("Lose function")
+plt.show()
 
-print(x())
+
+#print(-np.log(0.00000001))
