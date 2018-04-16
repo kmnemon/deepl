76543f2479fe6580bc03e520983c8f79e29904e3
ke <kliu@ips.com>
2018-04-16 18:10:50 +0800

    main21

--- course21/main21.py
+++ course21/main21.py
@@ -32,17 +32,71 @@ def model(X, Y, learning_rate = 0.01, num_iterations = 1, print_cost = True, ini
     Returns:
     parameters -- parameters learnt by the model
     """
-    layer_dims
+    grads = {}
+    costs = []  # to keep track of the loss
+    m = X.shape[1]  # number of examples
+    layers_dims = [X.shape[0], 10, 5, 1]
+
+    # Initialize parameters dictionary.
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        a3, cache = forward_propagation(X, parameters)
+
+        # Loss
+        cost = compute_loss(a3, Y)
+
+        # Backward propagation.
+        grads = backward_propagation(X, Y, cache)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Print the loss every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+
+    # plot the loss
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
 
 
-    if initialization == "he":
-        parameters= initialization(layer_dims,  )
-    elif initialization == "zeros":
-        parameters = initializtion()
-    elif intialization == "random":
-        parameters = initializtion()
-    else:
-        assert( 1 == False)
+
+def initialize_parameters_zeros(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+
+    size_t = layers_dims.size()
+    for i in range(size_t):
+        np.zeros(shape)
+
+
+
+
 
 
 
