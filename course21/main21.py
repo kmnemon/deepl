dbb6553cbe4a48e9bdd5fe32ffd34062df46159c
ke <kliu@ips.com>
2018-04-16 19:42:41 +0800

    zero ini

--- course21/main21.py
+++ course21/main21.py
@@ -90,9 +90,18 @@ def initialize_parameters_zeros(layers_dims):
                     bL -- bias vector of shape (layers_dims[L], 1)
     """
 
-    size_t = layers_dims.size()
-    for i in range(size_t):
-        np.zeros(shape)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l-1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+
+        assert (parameters['W' + str(l)].shape == (layers_dims[l], layers_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layers_dims[l], 1))
+
+    return parameters
 
 
 
