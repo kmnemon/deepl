d97f989d78a51fa24d2ef3cb739718a0f6b2777a
kmnemon <koenliu@outlook.com>
2018-04-27 00:01:22 +0800

    dropout

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-W1 = [[1,2,3],
+W1 = np.array([[1,2,3],
       [4,5,6]
-      ]
+      ])
 
 
-a = np.sum(np.square(W1))
+W2 = np.random.rand(W1.shape[0], W1.shape[1])
 
-print(a)
+print(W2.shape)
