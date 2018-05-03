f2046e98b2edd1b0f0f7b88aa1fcf6e607eb2436
ke <kliu@ips.com>
2018-05-03 15:03:53 +0800

    merge devcode

---
+++ base/utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1/(1+np.exp(-x))
+    return s
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0,x)
+
+    return s
--- course21/main21_2.py
+++ course21/main21_2.py
@@ -198,17 +198,17 @@ def forward_propagation_with_dropout(X, parameters, keep_prob=0.5):
     A1 = relu(Z1)
     ### START CODE HERE ### (approx. 4 lines)         # Steps 1-4 below correspond to the Steps 1-4 described above.
     D1 = np.random.rand(A1.shape[0], A1.shape[1])  # Step 1: initialize matrix D1 = np.random.rand(..., ...)
-    D1 = None  # Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
-    A1 = None  # Step 3: shut down some neurons of A1
-    A1 = None  # Step 4: scale the value of neurons that haven't been shut down
+    D1 = D1 < keep_prob  # Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
+    A1 = A1 * D1  # Step 3: shut down some neurons of A1
+    A1 = A1 / keep_prob  # Step 4: scale the value of neurons that haven't been shut down
     ### END CODE HERE ###
     Z2 = np.dot(W2, A1) + b2
     A2 = relu(Z2)
     ### START CODE HERE ### (approx. 4 lines)
-    D2 = None  # Step 1: initialize matrix D2 = np.random.rand(..., ...)
-    D2 = None  # Step 2: convert entries of D2 to 0 or 1 (using keep_prob as the threshold)
-    A2 = None  # Step 3: shut down some neurons of A2
-    A2 = None  # Step 4: scale the value of neurons that haven't been shut down
+    D2 = np.random.rand(A2.shape[0], A2.shape[1])  # Step 1: initialize matrix D2 = np.random.rand(..., ...)
+    D2 = D2 < keep_prob  # Step 2: convert entries of D2 to 0 or 1 (using keep_prob as the threshold)
+    A2 = A2 * D2  # Step 3: shut down some neurons of A2
+    A2 = A2 / keep_prob  # Step 4: scale the value of neurons that haven't been shut down
     ### END CODE HERE ###
     Z3 = np.dot(W3, A2) + b3
     A3 = sigmoid(Z3)
@@ -218,6 +218,57 @@ def forward_propagation_with_dropout(X, parameters, keep_prob=0.5):
     return A3, cache
 
 
+# GRADED FUNCTION: backward_propagation_with_dropout
+
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+    """
+    Implements the backward propagation of our baseline model to which we added dropout.
+
+    Arguments:
+    X -- input dataset, of shape (2, number of examples)
+    Y -- "true" labels vector, of shape (output size, number of examples)
+    cache -- cache output from forward_propagation_with_dropout()
+    keep_prob - probability of keeping a neuron active during drop-out, scalar
+
+    Returns:
+    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+    """
+
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = 1. / m * np.dot(dZ3, A2.T)
+    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dA2 = dA2 * D2  # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
+    dA2 = dA2 / keep_prob  # Step 2: Scale the value of neurons that haven't been shut down
+    ### END CODE HERE ###
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
+    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dA1 = dA1 * D1  # Step 1: Apply mask D1 to shut down the same neurons as during the forward propagation
+    dA1 = dA1 / keep_prob  # Step 2: Scale the value of neurons that haven't been shut down
+    ### END CODE HERE ###
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1. / m * np.dot(dZ1, X.T)
+    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1,
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
 
 
+parameters = model(train_X, train_Y, keep_prob = 0.86, learning_rate = 0.3)
 
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
