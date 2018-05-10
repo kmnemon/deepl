2631aea1d775fc7cab29ec7ca276f247d6ca3815
ke <kliu@ips.com>
2018-05-10 11:00:34 +0800

    rename

--- improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
+++ improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
@@ -2,3 +2,5 @@ import numpy as np
 from datasets21.testCases import *
 from datasets21.gc_utils import dictionary_to_vector, vector_to_dictionary, gradients_to_vector
 from base.utils import sigmoid, relu
+
+
