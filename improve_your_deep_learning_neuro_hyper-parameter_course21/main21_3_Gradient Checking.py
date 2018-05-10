96d5381672c87e5e19de8df67491fb093f497c54
ke <kliu@ips.com>
2018-05-10 11:08:47 +0800

    modify folder

--- improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
+++ improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
@@ -1,6 +1,6 @@
 import numpy as np
-from datasets21.testCases import *
-from datasets21.gc_utils import dictionary_to_vector, vector_to_dictionary, gradients_to_vector
-from base.utils import sigmoid, relu
+from testCases import *
+from base.gc_utils import  dictionary_to_vector, vector_to_dictionary, gradients_to_vector\
+form base.utils import sigmoid, relu
 
 
