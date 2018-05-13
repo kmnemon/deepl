ee8552e2cbcc2b7993af8f56e406525ae2cf064c
kmnemon <koenliu@outlook.com>
2018-05-13 22:51:04 +0800

    df

--- improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
+++ improve_your_deep_learning_neuro_hyper-parameter_course21/main21_3_Gradient Checking.py
@@ -4,3 +4,56 @@ from base.gc_utils import  dictionary_to_vector, vector_to_dictionary, gradients
 form base.utils import sigmoid, relu
 
 
+#1D Gradient Checking
+def forward_propagation(x, theta):
+    """
+    Implement the linear forward propagation (compute J) presented in Figure 1 (J(theta) = theta * x)
+
+    Arguments:
+    x -- a real-valued input
+    theta -- our parameter, a real number as well
+
+    Returns:
+    J -- the value of function J, computed using the formula J(theta) = theta * x
+    """
+
+    ### START CODE HERE ### (approx. 1 line)
+    J = theta * x
+    ### END CODE HERE ###
+
+    return J
+
+
+# GRADED FUNCTION: backward_propagation
+
+def backward_propagation(x, theta):
+    """
+    Computes the derivative of J with respect to theta (see Figure 1).
+
+    Arguments:
+    x -- a real-valued input
+    theta -- our parameter, a real number as well
+
+    Returns:
+    dtheta -- the gradient of the cost with respect to theta
+    """
+
+    ### START CODE HERE ### (approx. 1 line)
+    dtheta = x
+    ### END CODE HERE ###
+
+    return dtheta
+
+
+def gradient_check(x, theta, epsilon = 1e-7):
+    """
+    Implement the backward propagation presented in Figure 1.
+
+    Arguments:
+    x -- a real-valued input
+    theta -- our parameter, a real number as well
+    epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+
+    Returns:
+    difference -- difference (2) between the approximated gradient and the backward propagation gradient
+    """
