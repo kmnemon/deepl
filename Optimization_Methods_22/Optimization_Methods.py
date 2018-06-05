79b2096bb3d4cb1dee460edae26a7e9a70c69e44
ke Liu <keliu@keiMac.local>
2018-06-05 18:15:07 +0800

    d

--- Optimization_Methods_22/Optimization_Methods.py
+++ Optimization_Methods_22/Optimization_Methods.py
@@ -40,3 +40,112 @@ def update_parameters_with_gd(parameters, grads, learning_rate):
         parameters['b' + str(l+1)] = parameters['b' + str(l+1)] - learning_rate * grads['db' + str(l+1)]
 
     return parameters
+
+
+# GRADED FUNCTION: random_mini_batches
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+    """
+    Creates a list of random minibatches from (X, Y)
+
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    mini_batch_size -- size of the mini-batches, integer
+
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+    np.random.seed(seed)
+    m = X.shape[1]
+    mini_batches = []
+
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1, m))
+
+
+    #Setp 2 mini batch, mini_batch_X(batch_num, X.shape[0], batch_size)
+    num_complete_minibatches = math.floor(m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size : (k+1) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size : (k+1) * mini_batch_size]
+
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    #Handling the end case (last mini-batch < mini_batch_size)
+    if( m % mini_batch_size != 0 ):
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size: m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size: m]
+
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+
+    return mini_batches
+
+
+# Momentum
+# GRADED FUNCTION: initialize_velocity
+def initialize_velocity(parameters):
+    """
+    Initializes the velocity as a python dictionary with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL"
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+
+    Returns:
+    v -- python dictionary containing the current velocity.
+                    v['dW' + str(l)] = velocity of dWl
+                    v['db' + str(l)] = velocity of dbl
+    """
+    L = len(parameters) // 2
+    v = {}
+
+
+    for l in range(L):
+        v['dW' + str(l + 1)] = np.zeros(parameters['W' + str(l + 1)].shape)
+        v['db' + str(l + 1)] = np.zeros(parameters['b' + str(l + 1)].shape)
+ #       v['dW' + str(l + 1)] = B1 * v['dW' + str(l + 1)] + (1 - B1) * v['dW' + str(l + 1)]
+ #       v['db' + str(l + 1)] = B1 * v['db' + str(l + 1)] + (1 - B1) * v['db' + str(l + 1)]
+
+    return v
+
+
+# GRADED FUNCTION: update_parameters_with_momentum
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    """
+    Update parameters using Momentum
+
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- python dictionary containing the current velocity:
+                    v['dW' + str(l)] = ...
+                    v['db' + str(l)] = ...
+    beta -- the momentum hyperparameter, scalar
+    learning_rate -- the learning rate, scalar
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    v -- python dictionary containing your updated velocities
+    """
+
+    L = len(parameters) // 2
+    for l in range(L):
+        v['dW' + str(l + 1)] = beta * v['dW' + str(l + 1)] + (1 - beta) * grads['dW' + str(l + 1)]
+        v['db' + str(l + 1)] = beta * v['db' + str(l + 1)] + (1 - beta) * grads['db' + str(l + 1)]
+
+        parameters['W' + str(l + 1)] = parameters['W' + str(l + 1)] - learning_rate * v['dW' + str(l + 1)]
+        parameters['b' + str(l + 1)] = parameters['b' + str(l + 1)] - learning_rate * v['db' + str(l + 1)]
+
+    return parameters, v
+
