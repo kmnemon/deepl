606e45cea3088efe33a3d39d0c9ba1036259d37f
ke Liu <keliu@keiMac.local>
2018-06-10 18:49:27 +0800

    d;

--- hyperparameter_tune_23/TensorFlow_tutorial_23.py
+++ hyperparameter_tune_23/TensorFlow_tutorial_23.py
@@ -6,7 +6,19 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
 
-%matplotlib inline
+
 np.random.seed(1)
 
 #https://blog.csdn.net/Koala_Tree/article/details/78254608?locationNum=4&fps=1
+
+y_hat = tf.constant(36, name='y_hat') # Define y_hat constant. Set to 36.
+y = tf.constant(39, name='y')
+
+loss = tf.Variable((y - y_hat) ** 2, name = 'loss')
+
+init = tf.global_variables_initializer()         # When init is run later (session.run(init)),
+                                                 # the loss variable will be initialized and ready to be computed
+
+with tf.Session() as session:                    # Create a session and print the output
+    session.run(init)                            # Initializes the variables
+    print(session.run(loss))                     # Prints the loss
