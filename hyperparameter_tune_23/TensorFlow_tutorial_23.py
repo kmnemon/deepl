28066a3958ff5716a6fafa2e4e73b8efb8edbf58
ke Liu <keliu@keiMac.local>
2018-06-11 20:36:59 +0800

    d

--- hyperparameter_tune_23/TensorFlow_tutorial_23.py
+++ hyperparameter_tune_23/TensorFlow_tutorial_23.py
@@ -7,8 +7,7 @@ from tensorflow.python.framework import ops
 from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
 
 
-np.random.seed(1)
-
+'''
 #https://blog.csdn.net/Koala_Tree/article/details/78254608?locationNum=4&fps=1
 
 y_hat = tf.constant(36, name='y_hat') # Define y_hat constant. Set to 36.
@@ -22,3 +21,95 @@ init = tf.global_variables_initializer()         # When init is run later (sessi
 with tf.Session() as session:                    # Create a session and print the output
     session.run(init)                            # Initializes the variables
     print(session.run(loss))                     # Prints the loss
+
+a = tf.constant(2)
+b = tf.constant(10)
+c = tf.multiply(a,b)
+sess = tf.Session()
+print(sess.run(c))
+
+
+sess = tf.Session()
+x = tf.placeholder(tf.int64, name = 'x')
+print(sess.run(2 * x, feed_dict = {x: 3}))
+sess.close()
+'''
+
+
+# GRADED FUNCTION: linear_function
+
+def linear_function():
+    """
+    Implements a linear function:
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns:
+    result -- runs the session for Y = WX + b
+    """
+
+    np.random.seed(1)
+
+    #Y=WX+b
+    W = tf.constant(np.random.randn(4,3), name = 'W')
+    X = tf.constant(np.random.randn(3,1), name = 'X')
+    b = tf.constant(np.random.randn(4,1), name = 'b')
+    Y = tf.add(tf.matmul(W, X), b)
+
+    sess = tf.Session()
+    result = sess.run(Y)
+
+    sess.close()
+
+    return result
+
+
+
+# GRADED FUNCTION: sigmoid
+def sigmoid(z):
+    """
+    Computes the sigmoid of z
+
+    Arguments:
+    z -- input value, scalar or vector
+
+    Returns:
+    results -- the sigmoid of z
+    """
+
+    x = tf.placeholder(tf.float32, name = 'x')
+    sz = tf.sigmoid(x)
+
+    sess = tf.Session()
+    sz = sess.run(sz, feed_dict={x:z})
+    sess.close()
+
+    return sz
+
+
+
+
+# GRADED FUNCTION: cost
+def cost(logits, labels):
+    """
+    Computes the cost using the sigmoid cross entropy
+
+    Arguments:
+    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
+    labels -- vector of labels y (1 or 0)
+
+    Note: What we've been calling "z" and "y" in this class are respectively called "logits" and "labels"
+    in the TensorFlow documentation. So logits will feed into z, and labels into y.
+
+    Returns:
+    cost -- runs the session of the cost (formula (2))
+    """
+
+
+
+
+
+
+
+
+
