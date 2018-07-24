8f3aadea661a56f171beea082cc8c8bd9bdd9303
kmnemon <koenliu@outlook.com>
2018-07-25 00:16:29 +0800

    d

--- neural_style_transfer_44_1/neural_style_transfer.py
+++ neural_style_transfer_44_1/neural_style_transfer.py
@@ -11,4 +11,106 @@ import tensorflow as tf
 
 
 model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
-print(model)
+content_image = scipy.misc.imread("images/louvre.jpg")
+
+
+# GRADED FUNCTION: compute_content_cost
+
+def compute_content_cost(a_C, a_G):
+    """
+    Computes the content cost
+
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+
+    Returns:
+    J_content -- scalar that you compute using equation 1 above.
+    """
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from a_G (≈1 line)
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape a_C and a_G (≈2 lines)
+    a_C_unrolled = tf.reshape(a_C, [n_H*n_W, n_C])
+    a_G_unrolled = tf.reshape(a_G, [n_H*n_W, n_C])
+
+    # compute the cost with tensorflow (≈1 line)
+    J_content = 1./(4 * n_H * n_W * n_C)*tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled)))
+    ### END CODE HERE ###
+
+    return J_content
+
+'''
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    tf.set_random_seed(1)
+    a_C = tf.random_normal([1, 4, 4, 3], mean=1, stddev=4)
+    a_G = tf.random_normal([1, 4, 4, 3], mean=1, stddev=4)
+    J_content = compute_content_cost(a_C, a_G)
+    print("J_content = " + str(J_content.eval()))
+'''
+
+style_image = scipy.misc.imread("images/monet_800600.jpg")
+
+
+# GRADED FUNCTION: gram_matrix
+
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+
+    ### START CODE HERE ### (≈1 line)
+    GA = tf.matmul(A, tf.transpose(A))
+    ### END CODE HERE ###
+
+    return GA
+
+'''
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    tf.set_random_seed(1)
+    A = tf.random_normal([3, 2*1], mean=1, stddev=4)
+    GA = gram_matrix(A)
+
+    print("GA = " + str(GA.eval()))
+'''
+
+
+# GRADED FUNCTION: compute_layer_style_cost
+
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+
+    Returns:
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from a_G (≈1 line)
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape the images to have them of shape (n_C, n_H*n_W) (≈2 lines)
+    a_S = tf.transpose(tf.reshape(a_S, [n_H*n_W, n_C]))
+    a_G = tf.transpose(tf.reshape(a_G, [n_H*n_W, n_C]))
+
+    # Computing gram_matrices for both images S and G (≈2 lines)
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss (≈1 line)
+    J_style_layer = 1. / ( 4 * np.square(n_C)  * np.square(n_H * n_W)) * tf.reduce_sum(tf.square(tf.subtract(GS, GG)))
+    ### END CODE HERE ###
+
+    return J_style_layer
